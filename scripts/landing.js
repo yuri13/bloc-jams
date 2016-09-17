6a1f8943772ea815d8ce03ce9abcfdb6a05ba278
Yuri Reyes <reyesyuri@yahoo.com>
2016-09-17 12:42:40 -0700

    Checkpoint 9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,7 @@
-var animatePoints = function () {
-    
-    var points = document.getElementsByClassName('point');
- 
+var pointArray = document.getElementsByClassName('point');
+
+var animatePoints = function (points) {
+     
     for (i = 0; i < points.length; i++) {
     
         var revealPoint = function (index) {
@@ -15,4 +15,21 @@ var animatePoints = function () {
         revealPoint(i);
     }
 };
+
+window.onload = function () {
+    
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function (event){
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
+
             
