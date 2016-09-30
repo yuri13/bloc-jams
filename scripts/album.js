dcb7655a4fce1602eb922c8073f30c4790f7471d
Yuri Reyes <reyesyuri@yahoo.com>
2016-09-30 12:54:23 -0700

    Assignment 19 Extra Credit

--- scripts/album.js
+++ scripts/album.js
@@ -114,53 +114,34 @@ var $nextButton = $('.main-controls .next');
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
-    $previousButton.click(previousSong);
-    $nextButton.click(nextSong);
+    $previousButton.click(prevNextSong);
+    $nextButton.click(prevNextSong);
 });
 
-var nextSong = function() {
-    
-    var getLastSongNumber = function(index) {
-        return index == 0 ? currentAlbum.songs.length : index;
-    };
-    
-    var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
-    currentSongIndex++;
-    
-    if (currentSongIndex >= currentAlbum.songs.length) {
-        currentSongIndex = 0;
-    }
-    
-    currentlyPlayingSongNumber = currentSongIndex + 1;
-    currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
 
-    $('.currently-playing .song-name').text(currentSongFromAlbum.title);
-    $('.currently-playing .artist-name').text(currentAlbum.artist);
-    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.title);
-    $('.main-controls .play-pause').html(playerBarPauseButton);
-    
-    var lastSongNumber = getLastSongNumber(currentSongIndex);
-    var $nextSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber);
-    var $lastSongNumberCell = getSongNumberCell(lastSongNumber);
-    
-    $nextSongNumberCell.html(pauseButtonTemplate);
-    $lastSongNumberCell.html(lastSongNumber);
-    
-};
 
-var previousSong = function() {
+var prevNextSong = function() {
     
-    
-    var getLastSongNumber = function(index) {
-        return index == (currentAlbum.songs.length - 1) ? 1 : index + 2;
-    };
-    
-    var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
-    currentSongIndex--;
-    
-    if (currentSongIndex < 0) {
-        currentSongIndex = currentAlbum.songs.length - 1;
-    }
+            if (currentSongIndex >= currentAlbum.songs.length) {
+                currentSongIndex = 0;
+                
+                var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+                currentSongIndex++;
+                
+                var getLastSongNumber = function(index) {
+                    return index == 0 ? currentAlbum.songs.length : index;
+                }
+                
+            } else if (currentSongIndex < 0) {
+                currentSongIndex = currentAlbum.songs.length - 1;
+                
+                var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+                currentSongIndex--;
+                
+                var getLastSongNumber = function(index) {
+                    return index == (currentAlbum.songs.length - 1) ? 1 : index + 2;
+                }
+            };
     
     currentlyPlayingSongNumber = currentSongIndex + 1;
     currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
